5f6bb0237683a238095473577cd71e36309c3cfe
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-21 17:06:29 +0200

    sum

---
+++ src/main/java/chromaconsole/color.java
@@ -0,0 +1,81 @@
+package chromaconsole;
+
+public class color {
+    public static String text(int r, int g, int b) {
+        if (ChromaConsole.enabled == true){
+            return String.format("\033[38;2;%d;%d;%dm", r, g, b);
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String text(String color) {
+        if (ChromaConsole.enabled == true){
+            // Remove any leading '#' if present
+            if (color.startsWith("#")) {
+                color = color.substring(1);
+            }
+
+            int r, g, b;
+
+            if (color.length() == 3) {
+                // Handle formats like "RGB" (e.g., "000")
+                r = Integer.parseInt(color.substring(0, 1) + color.substring(0, 1), 16);
+                g = Integer.parseInt(color.substring(1, 2) + color.substring(1, 2), 16);
+                b = Integer.parseInt(color.substring(2, 3) + color.substring(2, 3), 16);
+            } else if (color.length() == 6) {
+                // Handle formats like "RRGGBB" (e.g., "000000")
+                r = Integer.parseInt(color.substring(0, 2), 16);
+                g = Integer.parseInt(color.substring(2, 4), 16);
+                b = Integer.parseInt(color.substring(4, 6), 16);
+            } else {
+                throw new IllegalArgumentException("Invalid color format");
+            }
+
+            return text(r, g, b);
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String background(int r, int g, int b) {
+        if (ChromaConsole.enabled == true){
+            return String.format("\033[48;2;%d;%d;%dm", r, g, b);
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String background(String color) {
+        if (ChromaConsole.enabled == true){
+            // Remove any leading '#' if present
+            if (color.startsWith("#")) {
+                color = color.substring(1);
+            }
+
+            int r, g, b;
+
+            if (color.length() == 3) {
+                // Handle formats like "RGB" (e.g., "000")
+                r = Integer.parseInt(color.substring(0, 1) + color.substring(0, 1), 16);
+                g = Integer.parseInt(color.substring(1, 2) + color.substring(1, 2), 16);
+                b = Integer.parseInt(color.substring(2, 3) + color.substring(2, 3), 16);
+            } else if (color.length() == 6) {
+                // Handle formats like "RRGGBB" (e.g., "000000")
+                r = Integer.parseInt(color.substring(0, 2), 16);
+                g = Integer.parseInt(color.substring(2, 4), 16);
+                b = Integer.parseInt(color.substring(4, 6), 16);
+            } else {
+                throw new IllegalArgumentException("Invalid color format");
+            }
+
+            return background(r, g, b);
+        }
+        else {
+            return "";
+        }
+    }
+}
---
+++ src/main/java/chromaconsole/style.java
@@ -0,0 +1,115 @@
+package chromaconsole;
+
+public class style {
+    public static boolean enabled = true;
+
+    public static void disable() {
+        ChromaConsole.enabled = false;
+    }
+
+    public static void enable() {
+        ChromaConsole.enabled = true;
+    }
+
+    public static String bold() {
+        if (enabled == true){
+            return "\033[1m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String italic() {
+        if (enabled == true){
+            return "\033[3m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String underline() {
+        if (enabled == true){
+            return "\033[4m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String strikethrough() {
+        if (enabled == true){
+            return "\033[9m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String negative() {
+        if (enabled == true){
+            return "\033[7m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String normal() {
+        if (enabled == true){
+            return "\033[22m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String reset() {
+        if (enabled == true){
+            return "\033[0m";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static String minecraft(String... args) {
+        if (enabled == true){
+            String symbol = "§";
+
+            String[] codes = {
+                "0", color.text("#000"),
+                "1", color.text("#00a"),
+                "2", color.text("#0a0"),
+                "3", color.text("#0aa"),
+                "4", color.text("#a00"),
+                "5", color.text("#a0a"),
+                "6", color.text("#fa0"),
+                "7", color.text("#aaa"),
+                "8", color.text("#555"),
+                "9", color.text("#55f"),
+                "a", color.text("#5f5"),
+                "b", color.text("#5ff"),
+                "c", color.text("#f55"),
+                "d", color.text("#f5f"),
+                "e", color.text("#ff5"),
+                "f", color.text("#fff"),
+                "l", bold(),
+                "m", strikethrough(),
+                "n", underline(),
+                "o", italic(),
+                "r", reset()
+            };
+
+            String text = args.length == 2 ? args[1] : args[0];
+            for (int i = 0; i < codes.length; i += 2) {
+                text = text.replace(symbol + codes[i], codes[i + 1]);
+            }
+            return text;
+        }
+        else {
+            return "";
+        }
+    }
+}
